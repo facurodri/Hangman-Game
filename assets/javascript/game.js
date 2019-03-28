adc1263d9f8bf00654cff5b050a71de20b7a18c1
Facundo Rodriguez <facurodri91@gmail.com>
2019-03-28 16:48:46 -0400

    fixed word input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,11 +5,10 @@
 // check player input is in computers choice
 // if it is NOT store the letter in array of fails and deduct 1 point from turn
 
-var names = ["berni", "facu", "phil", "adam"];
+var names = ["berni", "facu", "phil", "adam", "stanley matthews"];
 var compRandom;
 var usedletters = [];
 var spacesArr = [];
-var guessedLetter = [];
 var wrongLetters = [];
 var tries = 5;
 var wins = 0;
@@ -25,28 +24,21 @@ function initializeProgram() {
     fullName = [];
     for (i = 0; i < compRandom.length; i++) {
         spacesArr[i] = "_";
-        console.log(spacesArr);
     }
-    //$("#nameArr").text(spacesArr);
     console.log(compRandom);
 }
 
 function startGame() {
     initializeProgram();
     $("#startBtn").hide();
+    $("#restartBtn").hide();
     tries = 5;
     $(".keepscore").show();
+    $("#chances-left").show();
     $(".wordSection").show();
     $("#chances-left").text(tries);
     $("#guessed-letters").text("");
     $("#nameArr").text(spacesArr);
-}
-function restartGame() {
-
-    $("#restartBtn").hide();
-    $("#chances-left").text(tries);
-    $("#nameArr").text("");
-    initializeProgram();
 }
 
 //this line creates listner function (checkKeyPress) everytime a keyup event happens. "Keyup" is my parameter
@@ -55,17 +47,16 @@ document.addEventListener("keyup", checkKeyPress, false);
 // function validates to make sure they typed A-Z key otherwise it alerts its not valid key
 
 function checkKeyPress(stroke) {
-
+    
     if (stroke.keyCode >= "65" && stroke.keyCode <= "90") {
 
         checkForLetter(stroke.key);
-        console.log(stroke.key);
-
+        console.log(checkForLetter);
         usedletters.push(checkForLetter);
 
     } else {
         alert("That is not a letter! Try again using A-Z");
-        console.log(stroke.key);
+        
     }
 }
 
@@ -77,30 +68,19 @@ function checkForLetter(letter) {
     if (compRandom.includes(letter)) {
 
         for (i = 0; i < compRandom.length; i++) {
-            // console.log(compRandom);
+            
             if (letter == compRandom[i]) {
-                debugger;
-                // console.log(compRandom[i]);
+               
                 spacesArr[i] = letter;
-                console.log(spacesArr[i]);
-
-
-                if (letter === spacesArr[i]) {
-
-                    // spacesArr.push([i]);
-                    console.log(spacesArr);
-                    guessedLetter.push(letter);
-                    console.log(guessedLetter);
-                }
+                
                 $("#nameArr").text(spacesArr);
-                console.log(spacesArr);
+               
 
-                fullName[i] = letter;
+                fullName.push(letter);
                 console.log(fullName);
             }
         }
         if (compRandom.length === fullName.length) {
-            console.log("you win");
             wins++;
             //show win
             $("#wins").text(wins);
@@ -109,20 +89,19 @@ function checkForLetter(letter) {
 
         }
     } else {
-        tries -= 1;
-        debugger;
-        wrongLetters.push(letter);
+        if (!wrongLetters.includes(letter)){
+            tries -= 1;
+            wrongLetters.push(letter);
+        }
         $("#guessed-letters").text(wrongLetters);
 
-
-        // $("#guessed-letters").text(wrongLetters);
         $("#chances-left").text(tries);
 
         if (tries === 0) {
+            $("#chances-left").hide();
             // alert("You lose");
             $("#nameArr").text(compRandom);
             loses++;
-            debugger;
             $("#loses").text(loses);
             $("#restartBtn").show();
 
