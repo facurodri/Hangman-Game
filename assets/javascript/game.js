2597ef80ef781ffd8ce2f79858aa26c2dad5ba11
Facundo Rodriguez <facurodri91@gmail.com>
2019-04-09 11:36:48 -0400

    working on fixing sstuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,10 @@
 // create an array of word bank 
 // get random word from array
 // initialize array with spaces & dash based on the length of word-answersArr
-// create foreach ("_,") spaces
 // check player input is in computers choice
 // if it is NOT store the letter in array of fails and deduct 1 point from turn
 
-var names = ["berni", "facu", "phil", "adam", "stanley matthews"];
+var names = ["Lionel Messi", "cristiano ronaldo", "Micheal Platini", "Luka Modric", "stanley matthews"];
 var compRandom;
 var usedletters = [];
 var spacesArr = [];
@@ -19,16 +18,34 @@ var fullName = [];
 // & creates empty array with underscore 
 function initializeProgram() {
     compRandom = (names[Math.floor(Math.random() * names.length)]);
+    compRandom = compRandom.toUpperCase();
     spacesArr = [];
     wrongLetters = [];
     fullName = [];
+    emptyName = [];
+    
+    emptyName = compRandom.split("");
 
-        for (i = 0; i < compRandom.length; i++) {
-            spacesArr[i] = "_";
-        }
-        console.log(compRandom);
+    console.log(emptyName);
+    
+    for (i = 0; i < emptyName.length; i++) {
+       
+        if(emptyName[i] !== " "){
+            spacesArr[i] = ("_");
+            
+        } else {
+            spacesArr[i] = " ";          
+        }  
+    }   
+    
+    emptyName.push(spacesArr);
+    
+    emptyName = spacesArr.join(" ");
+              
+    // console.log(emptyName);
+    console.log(compRandom);
+    
     }
-
 
 function startGame() {
     initializeProgram();
@@ -40,7 +57,7 @@ function startGame() {
     $(".wordSection").show();
     $("#chances-left").text(tries);
     $("#guessed-letters").text("");
-    $("#nameArr").text(spacesArr);
+    $("#nameArr").text(emptyName);
 }
 
 //this line creates listner function (checkKeyPress) everytime a keyup event happens. "Keyup" is my parameter
@@ -51,10 +68,11 @@ document.addEventListener("keyup", checkKeyPress, false);
 function checkKeyPress(stroke) {
 
     if (stroke.keyCode >= "65" && stroke.keyCode <= "90") {
-
-        checkForLetter(stroke.key);
-        console.log(checkForLetter);
+    
+        checkForLetter(stroke.key.toUpperCase());
+        // console.log(checkForLetter);
         usedletters.push(checkForLetter);
+       
 
     } else {
         alert("That is not a letter! Try again using A-Z");
@@ -65,35 +83,42 @@ function checkKeyPress(stroke) {
 //function checks for letter in word/location & updates spaceArr
 //switching space for letter
 function checkForLetter(letter) {
-
-
+    
+    // letter = letter.toUpperCase();
     if (compRandom.includes(letter)) {
 
         for (i = 0; i < compRandom.length; i++) {
-
-            if (letter == compRandom[i]) {
-
+            if (letter.toUpperCase() == compRandom[i]) {
+                
                 spacesArr[i] = letter;
-
                 $("#nameArr").text(spacesArr);
+                
+                fullName.push(spacesArr[i]);
 
-
-                fullName.push(letter);
-                console.log(fullName);
+                console.log(fullName.length);
+                console.log(spacesArr.length);
+                
+                
             }
         }
+
+        
         if (compRandom.length === fullName.length) {
-            wins++;
-            //show win
-            $("#wins").text(wins);
-            $("#guessed-letters").text('');
-            $("#restartBtn").show();
+            
+                console.log("winner")
+                wins++;
+                //show win
+                $("#nameArr").text(compRandom);
+                $("#wins").text(wins);
+                $("#guessed-letters").text('');
+                $("#restartBtn").show();
+            
 
         }
     } else {
         if (!wrongLetters.includes(letter)) {
             tries -= 1;
-            wrongLetters.push(letter);
+            wrongLetters.push(letter.toUpperCase());
         }
         $("#guessed-letters").text(wrongLetters);
 
@@ -101,7 +126,6 @@ function checkForLetter(letter) {
 
         if (tries === 0) {
             $("#chances-left").hide();
-            // alert("You lose");
             $("#nameArr").text(compRandom);
             loses++;
             $("#loses").text(loses);
